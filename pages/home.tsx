3c145a26a539f6accb1efd8ef206a7e042976c38
Nicholas <nicholas@nicholass-air.lan>
2024-05-06 00:14:08 -0600

    added temp data

--- pages/home.tsx
+++ pages/home.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import SidePannel from '@/components/side_pannel';
 import CenterPannel from '@/components/structure';
 import Message from '@/components/messages'
+import User from '@/components/users'
 
 function Home() {
     const centerCont = <>
                             <div id="log">
-                            <Message text='asdf' avatar='' author='Caipo' /> 
+                            <Message text='asdf' avatar='https://i.imgur.com/fRvjCez.png' author='Caipo' /> 
                             </div>
                         </> 
     
     const sideCont = <> 
                         <div className="right-container">
                         <h3>Online Users</h3>
-                        <ul id="user-list"></ul>
+                        <ul id="user-list">
+                        {GetUsers()}
+                        </ul>
                         </div>
                     </>;
 
@@ -23,6 +26,19 @@ function Home() {
           <SidePannel content={sideCont} title='Users' />
     </>
   );
+}
+
+function GetUsers(){
+    const tempData = [
+    {'name' : 'CyberNinja42', 'avatar' : 'https://i.imgur.com/nfB8nHU.jpeg'}, 
+    {'name' : 'PixelPirate23', 'avatar' : 'https://i.imgur.com/fRvjCez.png'}, 
+    {'name' : 'CodeMasterX', 'avatar' : 'https://i.imgur.com/sWVxwkV.jpeg'}, 
+    {'name' : 'GeekGoddess', 'avatar' : 'https://i.imgur.com/IPVUvBD.jpeg'}, 
+]; 
+
+    
+    return tempData.map((o, i) => <User name={o['name']} avatar={o['avatar']} />);
+    
 }
 
 export default Home;
